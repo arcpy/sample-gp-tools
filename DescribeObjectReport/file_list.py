3a57884e9a76e4887d7eebd50404449c0f66d8bf
Andrew Ortego <DrewOrtegoGIS@gmail.com>
2016-08-25 11:55:32 -0700

    Uploaded Project
    Added .py files, sample output, and readme.md

---
+++ DescribeObjectReport/file_list.py
@@ -0,0 +1,7 @@
+user_files = [
+    # ENTER FILE NAMES HERE!
+    u'C:\\arcgis\\ArcTutor\\Editing\\Zion.gdb\\Research_areas',
+    u'C:\\arcgis\\ArcTutor\\Editing\\Zion.gdb',
+    u'C:\\arcgis\\ArcTutor\\Editing\\Hillshade.png',
+    u'C:\\arcgis\\ArcTutor\\CAD\ManhattanKS\\MillerRanch.dwg',
+]
